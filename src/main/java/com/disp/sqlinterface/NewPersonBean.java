3bf070897d047abb72dfac094ac817c3d2947714
Rami <rami.torkhani@gmail.com>
2016-10-06 15:43:52 +0200

    cleaning

--- src/main/java/com/disp/sqlinterface/NewPersonBean.java
+++ src/main/java/com/disp/sqlinterface/NewPersonBean.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class NewPersonBean {
 	public void person (){
-		Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Enter a userID: ");
-		int n = reader.nextInt(); // Scans the next token of the input as an int.
+
 	}
 }
